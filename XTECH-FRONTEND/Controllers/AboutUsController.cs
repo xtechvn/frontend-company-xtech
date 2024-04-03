648033f209b8554b4b520d038a6fffdfb8efc3a2
Hiếu <anhhieuk50@gmail.com>
2024-04-03 10:25:28 +0700

    #update

---
+++ XTECH-FRONTEND/Controllers/AboutUsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace XTECH_FRONTEND.Controllers
+{
+    public class AboutUsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ XTECH-FRONTEND/Controllers/NewsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace XTECH_FRONTEND.Controllers
+{
+    public class NewsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public async Task<IActionResult> Detail(long id)
+        {
+            return View();
+
+        }
+    }
+}
