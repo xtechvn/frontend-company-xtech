4dd40c5ca5693bd370a660a72c7aa7f4cf7c2d0b
Hiếu <anhhieuk50@gmail.com>
2024-04-08 09:39:01 +0700

    update

--- XTECH-FRONTEND/wwwroot/modules/News.js
+++ XTECH-FRONTEND/wwwroot/modules/News.js
@@ -5,6 +5,7 @@ var pageAllFareCurrent = 1;
 var paginateArticles = [];
 var selectedCategory = "";
 var selectedCategoryId = 0;
+let pagenum = 1;
 $(document).ready(function () {
 
     var path = window.location.pathname;
@@ -15,18 +16,22 @@ $(document).ready(function () {
 
 
 function getNewsCategory(page) {
+    
     var requestObj = {
         page: page == null ? 1 : page,
         size: size =  20 ,
         category_id: 2,
     }
+    pagenum = requestObj.page;
     var rows = "";
+    var pagination = "";
+  
     $.ajax({
         url: "/News/GetlistNews",
         type: "Post",
         data: { requestObj },
         success: function (result) {
-            if (result.data != null) {
+            if (result != undefined && result.data != null) {
                 for (var i in result.data) {
                     var item = result.data[i];
                   
@@ -52,6 +57,27 @@ function getNewsCategory(page) {
                                 </div>
                             </div>`
                 }
+                var total_page = Math.ceil(result.total_item / requestObj.size);
+                for (var i = 1; i <= total_page; i++) {
+                    pagination += `<li data-page="${i}" class="page-${i} page-item ${i == pagenum ? 'active' : ''}"><a onclick="getNewsCategory(${i})" class="page-link" href="javascripts:;">${i}</a></li>`;
+                }
+                var paginationHtml = `<ul class="pagination mb40">
+                                        <li class="page-item">
+                                            <a class="page-link" href="javascripts:;"onclick="getNewsCategory(1)" aria-label="Previous">
+                                                <span aria-hidden="true"><i class="fa fa-angle-left"></i></span>
+                                                <span class="sr-only">Previous</span>
+                                            </a>
+                                        </li>
+                                       ${pagination}
+                                        <li class="page-item">
+                                            <a class="page-link" href="javascripts:;" onclick="getNewsCategory(${total_page})" aria-label="Next">
+                                                <span aria-hidden="true"><i class="fa fa-angle-right"></i></span>
+                                                <span class="sr-only">Next</span>
+                                            </a>
+                                        </li>
+                                    </ul>`;
+              
+                $('#grid_data_pagination').html(paginationHtml);
                 $('#grid_data_New').html(rows);
             }
         }
