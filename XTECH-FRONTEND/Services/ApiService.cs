d62e05f7b1788d94f2f041d140c3643dca650eff
Hiếu <anhhieuk50@gmail.com>
2024-04-20 12:12:22 +0700

    get price vps

--- XTECH-FRONTEND/Services/ApiService.cs
+++ XTECH-FRONTEND/Services/ApiService.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.Net.Http;
+using System.Runtime.Intrinsics.X86;
 using XTECH_FRONTEND.Infrastructure.Utilities.Constants;
 using XTECH_FRONTEND.Infrastructure.Utilities.Helpers;
 using XTECH_FRONTEND.Models;
@@ -180,7 +181,47 @@ namespace XTECH_FRONTEND.Services
             }
         }
         #endregion news 
+        #region galaxy
+        public async Task<BaseObjectResponse<ProductGroupViewModel>> GetPriceGalaxy(int CPU, int Memory, int SSD, int net, int nip, int nMonth ,int quantity)
+        {
+            try
+            {
 
+                HttpClient _httpClient = new HttpClient();
+                BaseObjectResponse<ProductGroupViewModel> result = null;
+                var PrivateKey = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetSection("API")["KEY"];
+                var j_param = new Dictionary<string, long>()
+                {
+                    {"category_id",1007 },
+                };
+                var data = JsonConvert.SerializeObject(j_param);
+                var token = AdavigoHelper.Encode(data, PrivateKey);
+                var request = new[]
+                {
+                    new KeyValuePair<string, string>("token", token)
+                };
+                string a = "type = vps & cpu = "+ CPU + "& mem ="  +Memory+ " & ssd = " +SSD +"& net ="+ net+ "& nip =" +nip +"& nMonth =" +nMonth +"& quantity ="+ quantity;
+                var url = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetSection("API")["Domain"] + SystemConstants.AdavigoApiRoutes.getproductcategorybyparentid;
 
+                HttpResponseMessage response = await _httpClient.PostAsync(url, new FormUrlEncodedContent(request));
+
+                var stringResult = "";
+                if (response.IsSuccessStatusCode)
+                {
+                    stringResult = await response.Content.ReadAsStringAsync();
+                    result = JsonConvert.DeserializeObject<BaseObjectResponse<ProductGroupViewModel>>(stringResult);
+
+                    // send error log
+
+                }
+                return result;
+            }
+            catch (Exception ex)
+            {
+                LogHelper.InsertLogTelegram("getproductcategorybyparentid - ApiService: " + ex);
+                throw new Exception(ex.Message);
+            }
+        }
+        #endregion galaxy 
     }
 }
