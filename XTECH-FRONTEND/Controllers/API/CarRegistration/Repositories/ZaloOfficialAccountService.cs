d4916ea6bb8ef8477e5fb0d71126b8d9748f19d8
Hiếu <anhhieuk50@gmail.com>
2025-06-30 17:34:26 +0700

    udpate

--- XTECH-FRONTEND/Controllers/API/CarRegistration/Repositories/ZaloOfficialAccountService.cs
+++ XTECH-FRONTEND/Controllers/API/CarRegistration/Repositories/ZaloOfficialAccountService.cs
@@ -191,7 +191,7 @@ namespace XTECH_FRONTEND.Controllers.API.CarRegistration.Repositories
         {
             var message = $"🎉 XIN CHÀO {userDetail.display_name.ToUpper()}!\n\n" +
                          $"✅ ĐĂNG KÝ XE THÀNH CÔNG!\n\n" +
-                         $"🙏 Tên khách hàng: {record.Name}\n" +
+                         $" Tên khách hàng: {record.Name}\n" +
                          $"📱 Số điện thoại: {record.PhoneNumber}\n" +
                          $"🚗 Biển số xe: {record.PlateNumber}\n" +
                          $"🚗 Trọng tải xe: {record.Referee}\n" +
